63adf087bcb1cb69e9785887117bd0f59ad5c691
Asaf Pala <asaf.pala@gmail.com>
2025-09-01 01:22:56 +0300

    added .gitignore file

--- obj/Debug/net9.0/HelloApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9666d0d58969a28f997227880ed1e541046d32a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
